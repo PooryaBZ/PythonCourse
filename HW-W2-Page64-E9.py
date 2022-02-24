bf65cbc97e1898f0b9a3c8a40a1eb6f13e62df49
Poorya Bazargan <99671587+PooryaBZ@users.noreply.github.com>
2022-02-24 11:28:29 +0330

    Add files via upload

---
+++ HW-W2-Page64-E9.py
@@ -0,0 +1,113 @@
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+
+a = i1 + (i2 * i3)
+print("a)", end=" ")
+print(a, end=" ")
+print(type(a))
+
+b = i1 * (i2 + i3)
+print("b)", end=" ")
+print(b, end=" ")
+print(type(b))
+
+
+c = i1 / (i2 + i3)
+print("c)", end=" ")
+print(c, end=" ")
+print(type(c))
+
+
+d = i1 // (i2 + i3)
+print("d)", end=" ")
+print(d, end=" ")
+print(type(d))
+
+
+e = i1 / i2 + i3
+print("e)", end=" ")
+print(e, end=" ")
+print(type(e))
+
+
+f = i1 // i2 + i3
+print("f)", end=" ")
+print(f, end=" ")
+print(type(f))
+
+
+g = 3 + 4 + 5 / 3
+print("g)", end=" ")
+print(g, end=" ")
+print(type(g))
+
+
+h = 3 + 4 + 5 // 3
+print("h)", end=" ")
+print(h, end=" ")
+print(type(h))
+
+
+i = (3 + 4 + 5) / 3
+print("i)", end=" ")
+print(i, end=" ")
+print(type(i))
+
+
+j = (3 + 4 + 5) // 3
+print("j)", end=" ")
+print(j, end=" ")
+print(type(j))
+
+
+k = d1 + (d2 * d3)
+print("k)", end=" ")
+print(k, end=" ")
+print(type(j))
+
+
+l = d1 + d2 * d3
+print("l)", end=" ")
+print(l, end=" ")
+print(type(l))
+
+
+m = d1 / d2 - d3
+print("m)", end=" ")
+print(m, end=" ")
+print(type(m))
+
+
+n = d1 / (d2 - d3)
+print("n)", end=" ")
+print(n, end=" ")
+print(type(n))
+
+
+o = d1 + d2 + d3 / 3
+print("o)", end=" ")
+print(o, end=" ")
+print(type(o))
+
+
+p = (d1 + d2 + d3) / 3
+print("p)", end=" ")
+print(p, end=" ")
+print(type(p))
+
+
+q = d1 + d2 + (d3 / 3)
+print("q)", end=" ")
+print(q, end=" ")
+print(type(q))
+
+
+r = 3 * (d1 + d2) * (d1 - d3)
+print("r)", end=" ")
+print(r, end=" ")
+print(type(r))
+
