f53c7f5113e27e9c26f13f163f9d7a2ce031bfc5
Poorya Bazargan <99671587+PooryaBZ@users.noreply.github.com>
2022-02-27 12:50:04 +0330

    Add files via upload

---
+++ HW-W2-Page63-E7.py
@@ -0,0 +1,133 @@
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+
+a = i1 + i2
+print("a)", end=" ")
+print(a, end=" ")
+print(type(a))
+
+
+b = i1 / i2
+print("b)", end=" ")
+print(b, end=" ")
+print(type(b))
+
+
+c = i1 // i2
+print("c)", end=" ")
+print(c, end=" ")
+print(type(c))
+
+
+d = i2 / i1
+print("d)", end=" ")
+print(d, end=" ")
+print(type(d))
+
+
+e = i1 / i2 + i3
+print("e)", end=" ")
+print(e, end=" ")
+print(type(e))
+
+
+f = i1 * i3
+print("f)", end=" ")
+print(f, end=" ")
+print(type(f))
+
+
+g = d1 + d2
+print("g)", end=" ")
+print(g, end=" ")
+print(type(g))
+
+
+h = d1 / d2
+print("h)", end=" ")
+print(h, end=" ")
+print(type(h))
+
+
+i = d2 / d1
+print("i)", end=" ")
+print(i, end=" ")
+print(type(i))
+
+
+j = d3 * d1
+print("j)", end=" ")
+print(j, end=" ")
+print(type(j))
+
+
+k = d1 + i2
+print("k)", end=" ")
+print(k, end=" ")
+print(type(j))
+
+
+l = i1 / d2
+print("l)", end=" ")
+print(l, end=" ")
+print(type(l))
+
+
+m = d2 / i1
+print("m)", end=" ")
+print(m, end=" ")
+print(type(m))
+
+
+n = i2 / d1
+print("n)", end=" ")
+print(n, end=" ")
+print(type(n))
+
+
+o = i1 / i2 * d1
+print("o)", end=" ")
+print(o, end=" ")
+print(type(o))
+
+
+p = d1 * i1 / i2
+print("p)", end=" ")
+print(p, end=" ")
+print(type(p))
+
+
+q = d1 / d2 * i1
+print("q)", end=" ")
+print(q, end=" ")
+print(type(q))
+
+
+r = i1 * d1 / d2
+print("r)", end=" ")
+print(r, end=" ")
+print(type(r))
+
+s = i2 / i1 * d1
+print("s)", end=" ")
+print(s, end=" ")
+print(type(s))
+
+t = d1 * i2 / i1
+print("t)", end=" ")
+print(t, end=" ")
+print(type(t))
+
+u = d2 / d1 * i1
+print("u)", end=" ")
+print(u, end=" ")
+print(type(u))
+
+v = i1 * d2 / d1
+print("v)", end=" ")
+print(v, end=" ")
+print(type(v))
